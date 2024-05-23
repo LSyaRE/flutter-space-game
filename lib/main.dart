47cf4cabc3a9c2df0043e29ba085303787ac9963
LSyaRE <jonasvillacis@yahoo.com>
2024-05-23 07:28:53 -0500

    step1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
@@ -5,4 +6,24 @@ void main() {
   runApp(GameWidget(game: SpaceShooterGame()));
 }
 
-class SpaceShooterGame extends FlameGame {}
+class SpaceShooterGame extends FlameGame {
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+    add(Player()
+      ..position = size / 2
+      ..width = 50
+      ..height = 50
+      ..anchor = Anchor.center,
+      );
+  }
+}
+
+class Player extends PositionComponent {
+  static final _paint = Paint()..color = Colors.white;
+
+  @override
+  void render(Canvas canvas) {
+    canvas.drawRect(size.toRect(), _paint);
+  }
+}
